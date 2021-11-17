efb8fadb00ec3b37d585b45c2bcbb50ba25cd385
Guilherme Tod <guilhermetod@gmail.com>
2021-11-17 19:38:21 -0300

    feat!: remove [skip-ci]

--- release.config.js
+++ release.config.js
@@ -4,8 +4,12 @@ module.exports = {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
-    ['@semantic-release/npm', { npmPublish: false }],
-    '@semantic-release/git',
+    ['@semantic-release/npm', {
+       npmPublish: false,
+    }],
+    ['@semantic-release/git', {
+      message: 'chore(release): ${nextRelease.version} \n\n${nextRelease.notes}',
+    }],
     '@semantic-release/github',
   ],
 }
