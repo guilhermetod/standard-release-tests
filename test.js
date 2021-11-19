13d403e79472df9099b23f48db7ae4b72e48d724
Guilherme Tod <guilhermetod@gmail.com>
2021-11-19 18:26:27 -0300

    fix: test

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const semanticRelease = require('semantic-release')
 
-semanticRelease({}, {env: {
+semanticRelease({ci: false, dryRun: true}, {env: {
   ...process.env,
   GITHUB_EVENT: false,
 }}).then(r => {console.log(r.nextRelease.notes)})
