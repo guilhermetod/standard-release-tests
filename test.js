ee783179e25a83e86f31050e3c75071157144230
Guilherme Tod <guilhermetod@gmail.com>
2021-11-19 18:40:39 -0300

    fix: test

--- test.js
+++ test.js
@@ -9,8 +9,10 @@ const {execSync} = require('child_process')
 // }}).then(r => {console.log(r.nextRelease.notes)})
 
 const ci = envci({
-  ...process.env,
-  GITHUB_EVENT: false,
+  env: {
+    ...process.env,
+    GITHUB_EVENT: false,      
+  }
 })
 
 const execa = execSync('git rev-parse --abbrev-ref HEAD', {encoding: 'utf8'})
