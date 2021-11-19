c085d67e8ef0a3720eff6c9105c3394b5e742a3b
Guilherme Tod <guilhermetod@gmail.com>
2021-11-19 13:39:02 -0300

    fix: test

--- release.config.js
+++ release.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-  branches: [
-    { name: 'main' },
-    { name: 'devel', channel: 'pre/rc', prerelase: 'rc' },
-  ],
+  // branches: [
+  //   { name: 'main' },
+  //   { name: 'devel', channel: 'pre/rc', prerelase: 'rc' },
+  // ],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
-    ['@semantic-release/npm', { npmPublish: false }],
-    ['@semantic-release/git', { message: 'chore(release): ${nextRelease.version} \n\n${nextRelease.notes}' }],
-    '@semantic-release/github',
+    // ['@semantic-release/npm', { npmPublish: false }],
+    // ['@semantic-release/git', { message: 'chore(release): ${nextRelease.version} \n\n${nextRelease.notes}' }],
+    // '@semantic-release/github',
   ],
 }
