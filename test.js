ac3bc94ecff413194ac60e52d25b18d1a5cb3ff5
Guilherme Tod <guilhermetod@gmail.com>
2021-11-19 18:38:36 -0300

    fix: test

--- test.js
+++ test.js
@@ -1,7 +1,18 @@
-const semanticRelease = require('semantic-release')
+const envci = require('envci');
+const {execSync} = require('child_process')
+// const semanticRelease = require('semantic-release')
 
-semanticRelease({ci: false, dryRun: true}, {env: {
+// semanticRelease({ci: false, dryRun: true}, {env: {
+//   ...process.env,
+//   GITHUB_EVENT: false,
+//   GITHUB_ACTIONS: false
+// }}).then(r => {console.log(r.nextRelease.notes)})
+
+const ci = envci({
   ...process.env,
   GITHUB_EVENT: false,
-  GITHUB_ACTIONS: false
-}}).then(r => {console.log(r.nextRelease.notes)})
+})
+
+const execa = execSync('git rev-parse --abbrev-ref HEAD', {encoding: 'utf8'})
+
+console.log({ci, execa})
