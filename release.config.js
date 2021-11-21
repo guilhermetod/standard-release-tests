a1a216e707481c86d384a5eff2e4087493cd15a2
Guilherme Tod <guilhermetod@gmail.com>
2021-11-21 11:42:40 -0300

    feat: setup types

--- release.config.js
+++ release.config.js
@@ -1,26 +1,25 @@
+const  types = [
+  { type: 'Docs', release: 'patch' },
+  { type: 'Chore', release: 'patch' },
+  { type: 'CI', release: 'patch' },
+  { type: 'Feat', release: 'minor' },
+  { type: 'Fix', release: 'patch' },
+  { type: 'Perf', release: 'patch' },
+  { type: 'Refactor', release: 'patch' },
+  { type: 'Revert', release: 'patch' },
+  { type: 'Style', release: 'patch' },
+  { type: 'Test', release: 'patch' },
+  { type: 'WIP', release: 'patch' },
+];
+
 module.exports = {
   branches: [
     { name: 'main' },
     { name: 'devel', prerelase: 'beta' },
   ],
   plugins: [
-    ['@semantic-release/commit-analyzer', {
-      preset: 'angular',
-      releaseRules: [
-        { type: 'Docs', release: 'patch' },
-        { type: 'Chore', release: 'patch' },
-        { type: 'CI', release: 'patch' },
-        { type: 'Feat', release: 'minor' },
-        { type: 'Fix', release: 'patch' },
-        { type: 'Perf', release: 'patch' },
-        { type: 'Refactor', release: 'patch' },
-        { type: 'Revert', release: 'patch' },
-        { type: 'Style', release: 'patch' },
-        { type: 'Test', release: 'patch' },
-        { type: 'WIP', release: 'patch' },
-      ],
-    }],
-    '@semantic-release/release-notes-generator',
+    ['@semantic-release/commit-analyzer', { preset: 'conventionalcommits', releaseRules: types }],
+    ['@semantic-release/release-notes-generator', { preset: 'conventionalcommits' }],
     '@semantic-release/changelog',
     ['@semantic-release/npm', { npmPublish: false }],
     '@semantic-release/git',
