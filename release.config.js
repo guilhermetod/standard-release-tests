7e5308d0c88b4867a9580b496fcc082478a00b53
Guilherme Tod <guilhermetod@gmail.com>
2021-11-21 10:36:06 -0300

    Feat: configure titlecase

--- release.config.js
+++ release.config.js
@@ -4,7 +4,22 @@ module.exports = {
     { name: 'devel', prerelase: 'beta' },
   ],
   plugins: [
-    '@semantic-release/commit-analyzer',
+    ['@semantic-release/commit-analyzer', {
+      preset: 'angular',
+      releaseRules: [
+        { type: 'Docs', release: 'patch' },
+        { type: 'Chore', release: 'patch' },
+        { type: 'CI', release: 'patch' },
+        { type: 'Feat', release: 'minor' },
+        { type: 'Fix', release: 'patch' },
+        { type: 'Perf', release: 'patch' },
+        { type: 'Refactor', release: 'patch' },
+        { type: 'Revert', release: 'patch' },
+        { type: 'Style', release: 'patch' },
+        { type: 'Test', release: 'patch' },
+        { type: 'WIP', release: 'patch' },
+      ],
+    }],
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
     ['@semantic-release/npm', { npmPublish: false }],
