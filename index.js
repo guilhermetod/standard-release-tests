2422f210fd890de52670f47ecf9d4de80155a847
Guilherme Tod <guilhermetod@gmail.com>
2021-12-03 00:39:26 -0300

    Feat: allow preset

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const actionsCore = require('@actions/core');
 const semanticRelease = require('semantic-release');
 
 async function run() {
+  const preset = actionsCore.getInput('preset');
   const { nextRelease } = await semanticRelease({
      ci: false,
      dryRun: true,
-     plugins: ['@semantic-release/release-notes-generator'],
+     plugins: [['@semantic-release/release-notes-generator', { preset }]],
    }, {
     env: {
        ...process.env,
