a3af4fb6ff0feba62290568e6b0385901e3aff10
Guilherme Tod <guilhermetod@gmail.com>
2021-12-06 08:08:54 -0300

    Fix: build

--- src/core/output-preview.ts
+++ src/core/output-preview.ts
@@ -6,7 +6,7 @@ import semanticRelease from 'semantic-release';
 import { getPluginsConfig } from '@src/helpers/utils/get-plugins-config';
 
 async function dryRunRelease(): Promise<semanticRelease.Result> {
-  execSync(`git checkout${process.env.GITHUB_HEAD_REF}`);
+  execSync(`git checkout ${process.env.GITHUB_HEAD_REF}`);
   return semanticRelease({
     ci: false,
     dryRun: true,
